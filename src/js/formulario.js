a549aa7bdc16b15a8ea73a99c8c24db42dc06293
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2024-05-07 11:29:28 -0300

    Quest numero 2 do devQuest marcada estilizada e com logica conclusa feita em javaScript

---
+++ src/js/formulario.js
@@ -0,0 +1,34 @@
+const camposInput = document.querySelectorAll('.inputs')
+const btnSubmit = document.querySelector('.submit')
+const error = document.querySelectorAll('.error')
+
+btnSubmit.addEventListener('click', verificarCampos)
+
+
+/*
+
+// Aqui uma funcionalidade a mais para o codigo, ele verifica em tempo real se os inputs forem modificados
+
+camposInput.forEach(input => {
+    input.addEventListener('input', verificarCampos)
+})
+
+*/
+
+function verificarCampos() {
+    camposInput.forEach((input, index) => {
+        if (input.value === '') {
+            input.classList.remove('border')
+            input.classList.add('border-invalid')
+            error[index].style.display = 'block'
+        } else {
+            input.classList.remove('border-invalid')
+            input.classList.add('border-valid')
+            error[index].style.display = 'none'
+        }
+    })
+}
+
+
+
+
